0123828a84e67de4d92ae1b7fce7136a1b91f07b
figurebelow <ruben@figurebelow.com>
2017-11-07 08:08:40 +0000

    Pypi config details

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'yfm',
-  packages = ['yfm'], # this must be the same as the name above
+  packages = ['yfm'],
   version = '1.0',
   description = 'A command line tool to store and manage Yahoo Finance stock data in a MongoDb database',
   author = 'Ruben Afonso',
   author_email = 'rbfrancos@gmail.com',
-  url = 'https://github.com/rubenafo/yfm', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['yahoo-finance', 'mongodb', 'finance','stock'], # arbitrary keywords
+  url = 'https://github.com/rubenafo/yfMongo', 
+  download_url = 'https://github.com/rubenafo/yfMongo/archive/1.0.zip'
+  keywords = ['yahoo-finance', 'mongodb', 'finance','stock'],
   classifiers = [],
 )
 
